28b4e7e28bde28a5761ccf6a83eae0875376323b
arjun289 <arjun289singh@gmail.com>
2022-07-04 23:12:55 +0800

    Add files for volumes chapter

---
+++ volumes/fortune/fortuneloop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+trap "exit" SIGINT
+
+mkdir /var/htdocs
+while:
+do
+    echo $(date) Writing fortune to /var/htdocs/index.html
+    /usr/game/fortune > /var/htdocs/index.html
+    sleep 10
+done
